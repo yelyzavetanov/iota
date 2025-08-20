697c48d17dc49f97982dc56f1421ad159b09a424
yelyzavetanov <yelyzavetanov@gmail.com>
2025-08-20 22:05:25 +0300

    Remove .next directory from repo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../styles/globals.css";
+import "@/styles/globals.css";
+import "@/styles/base.css";
+import "@/styles/utilities.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
